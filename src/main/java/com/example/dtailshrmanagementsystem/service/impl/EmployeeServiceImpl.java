d9e5e64a71f777326888085d2a49315c028fc01e
sttsoneva <st.tsoneva@gmail.com>
2023-03-05 12:57:04 +0000

    Update EmployeeServiceImpl.java

--- src/main/java/com/example/dtailshrmanagementsystem/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/dtailshrmanagementsystem/service/impl/EmployeeServiceImpl.java
@@ -8,6 +8,9 @@ import com.example.dtailshrmanagementsystem.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
@@ -22,11 +25,68 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public void addNewEmployee() {
 
-        HomeAddress homeAddress = new HomeAddress("May Road", "Twickenham","London", "TW2 6RF", "UK");
-        Employee testEmployee = new Employee("Fred", "Flintstone", LocalDate.of(1987, 10, 19), "SS587259S",LocalDate.of(2021,4,21),"fred.flintstone@myworkemail.com", null, homeAddress);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter employee's first name");
+        String firstName = scan.nextLine();
+
+        System.out.println("Please enter employee's last name");
+        String lastName = scan.nextLine();
+
+        System.out.println("Please enter employee's Date of Birth");
+        LocalDate dateOfBirth = getLocalDate();
+
+        System.out.println("Please enter employee's National Insurance Number");
+        String nationalInsuranceNumber = scan.nextLine();
+
+        System.out.println("Please enter employee's start date");
+        LocalDate continuousServiceDate = getLocalDate();
+
+        System.out.println("Please enter employee's work email address");
+        String workEmail = scan.nextLine();
+
+        LocalDate dateLeft;
+
+        System.out.println("Please enter employee's home address");
+        System.out.println("First Line");
+        String firstLineOfAddress = scan.nextLine();
+
+        System.out.println("Second Line");
+        String secondLineOfAddress = scan.nextLine();
+
+        System.out.println("Town");
+        String thirdLineOfAddress = scan.nextLine();
+
+        System.out.println("Post Code");
+        String postCode = scan.nextLine();
+
+        System.out.println("Country");
+        String country = scan.nextLine();
+
+        HomeAddress homeAddress = new HomeAddress(firstLineOfAddress, secondLineOfAddress,thirdLineOfAddress, postCode, country);
+
+        Employee testEmployee = new Employee(firstName, lastName, dateOfBirth, nationalInsuranceNumber,continuousServiceDate,workEmail, null, homeAddress);
 
         homeAddressRepository.save(homeAddress);
         employeeRepository.save(testEmployee);
 
+    }
+
+    private LocalDate getLocalDate() {
+        Scanner scan = new Scanner(System.in);
+        LocalDate date = null;
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                System.out.println("Enter date in format yyyy-MM-dd");
+                String input = scan.nextLine();
+                date = LocalDate.parse(input);
+                validInput = true;
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid date format, please try again");
+            }
+        }
+        return date;
     }
 }
